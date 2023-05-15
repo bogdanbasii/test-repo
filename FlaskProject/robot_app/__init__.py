365b64dbecbc8c4cc0bc92130502322bcc324ca2
bogdanbasii <basiy.b@gmail.com>
2023-05-15 10:07:09 +0300

    fixed flask app and postgres

--- FlaskProject/robot_app/__init__.py
+++ FlaskProject/robot_app/__init__.py
@@ -1,19 +1,27 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from logging.config import dictConfig
 from .config import AppConfig
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
+app.secret_key = os.getenv('SECRET_KEY')
+app.config.from_object(AppConfig)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 dictConfig({
     'version': 1,
     'formatters': {'default': {
         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
     }}
-
 })
-
-app.config.from_object(AppConfig)
 
 from .views import *
 from .class_based_views import *
+from .models import *
+
+with app.app_context():
+    db.create_all()
---
+++ FlaskProject/robot_app/models.py
@@ -0,0 +1,20 @@
+from robot_app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    age = db.Column(db.Integer, nullable=False)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    year = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class Purchase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    date = db.Column(db.String)
