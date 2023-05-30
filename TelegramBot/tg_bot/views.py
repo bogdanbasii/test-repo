e00faa1cbd9bf37e0662af3b932bb94cff9a7d3a
bogdanbasii <basiy.b@gmail.com>
2023-05-30 21:57:07 +0300

    Final corrections. Removed security bot token leak in views.py

--- TelegramBot/tg_bot/views.py
+++ TelegramBot/tg_bot/views.py
@@ -2,9 +2,13 @@ import requests
 from tg_bot import app
 from flask import request
 from .handlers import TelegramHandler
+from dotenv import load_dotenv
+import os
 
-BOT_TOKEN = '5901762373:AAF5YvUzGor139zpNHZnWfQSBww38cQL_p8'
-TG_BASE_URL = 'https://api.telegram.org/bot'
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TG_BASE_URL = os.getenv("TG_BASE_URL")
 
 @app.route('/', methods=["POST"])
 def hello():
